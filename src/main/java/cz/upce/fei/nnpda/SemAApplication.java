3623aa7810650481afdb31675f82b8be8b00b1ec
Adam Dvořák <st67024@upce.cz>
2025-09-30 09:21:49 +0200

    initial commit

---
+++ src/main/java/cz/upce/fei/nnpda/SemAApplication.java
@@ -0,0 +1,13 @@
+package cz.upce.fei.nnpda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/upce/fei/nnpda/SemAApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.upce.fei.nnpda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
